c00fb563e9ca6f90e0e4de85bab688930bc3a8f9
terravidhal <infiniterraetincelle@gmail.com>
2024-05-29 05:17:52 +0100

    add courses pages

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Courses.css';
+import { Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 
 const Courses = () => {
@@ -7,7 +9,29 @@ const Courses = () => {
 
   return (
     <div className="Courses">
-      
+       <div className="title-course">Courses</div>
+       <div className="current-link">
+        <div className="current1">
+          <div className="current-img">
+            <img src="../src/assets/images/dashboard_gray.svg" alt="" />
+          </div>
+          <div className="current-name">Dashboard /</div>
+        </div>
+        <div className="current2">Courses</div>
+       </div>
+      <div className="home-courses">
+        <div className="table-top">
+          <div className="table-title">List of Courses</div>
+          <div className="btn-add">
+          <Button fontSize='16px'fontWeight={400} height='44px' mb={2}  display="flex" justifyContent="start" width='216px'
+            leftIcon={<img src="../src/assets/images/add.svg" alt="" />} 
+             colorScheme='#4C847B;'  
+             >
+              <Link to="/home">Request new course</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/utils/coursesData.js
@@ -0,0 +1,32 @@
+const coursesData = [
+  {
+    title: "Courses",
+    btnName: "View courses",
+  },
+  {
+    title: "Kids",
+    btnName: "Kids",
+  },
+  {
+    title: "Add Kid",
+    btnName: "Enroll Kid",
+  },
+  {
+    title: "Tutors",
+    btnName: "Tutors",
+  },
+  {
+    title: "Add Tutor",
+    btnName: "Enroll Tutor",
+  },
+  {
+    title: "Marketing",
+    btnName: "Marketing",
+  },
+  {
+    title: "Drive",
+    btnName: "Courses Drive",
+  },
+];
+
+export default coursesData;
