6c9cbd8d62c00492d166464cb0266a0da53c440f
terravidhal <infiniterraetincelle@gmail.com>
2024-05-29 14:24:33 +0100

    add datatable

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -2,6 +2,89 @@ import React from 'react';
 import './Courses.css';
 import { Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { Space, Table, Tag } from 'antd';
+
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '4',
+    name: 'Joe Black2',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
 
 
 const Courses = () => {
@@ -27,9 +110,12 @@ const Courses = () => {
             leftIcon={<img src="../src/assets/images/add.svg" alt="" />} 
              colorScheme='#4C847B;'  
              >
-              <Link to="/home">Request new course</Link>
+              <Link to="">Request new course</Link>
             </Button>
           </div>
+        </div>
+        <div className="data-tables">
+          <Table columns={columns} dataSource={data} pagination={{pageSize: 3,}} />
         </div>
       </div>
     </div>
